4640ba6b3dc4bf4dfdb6461925f1147adab788c6
DristiPatel <dristipatel@gmail.com>
2021-06-14 19:56:57 -0500

    added button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Header from "./component/header"
 import './App.css';
+import Button from '@material-ui/core/Button';
+
+// function addItem() {
+//   return (
+    
+//   );
+// }
 
 function App() {
   return (
     <div className="App">
     <Header/>
+    <Button>Add Item</Button>
     </div>
   );
 }
 
 export default App;
+
